51dcde1b745c48121c4d77af2e7d6b6091c7af29
Juyeon-0919 <janielee0919@domfam.com>
2020-09-09 23:06:20 +0900

    글로벌 리더십

--- pagelist/data/data.js
+++ pagelist/data/data.js
@@ -1,224 +1,241 @@
 var data = {}
 data.main = {
-	'title': '메인',
+	'title': '01_메인',
 	'list': [
 		{
 			'stateClass': 'complete',
 			'url': 'todayStudy',
-			'category': '메인',
+			'category': '01_메인',
 			'page': '01_메인_B2C_올인원'
 		},
 
 		{
 			'stateClass': 'complete',
 			'url': 'todayStudy_calendar',
-			'category': '메인',
+			'category': '01_메인',
 			'page': '01_메인_B2C_올인원_달력'
 		},
 
 		{
 			'stateClass': 'complete',
 			'url': 'mainStudyHall_college',
-			'category': '메인',
+			'category': '01_메인',
 			'page': '01_메인_학습관_단과'
 		},
 
 		{
 			'stateClass': 'complete',
 			'url': 'noStudyToday',
-			'category': '메인',
+			'category': '01_메인',
 			'page': '01_메인_B2C_학습없을때'
 		},
 		{
 			'stateClass': 'complete',
 			'url': 'mainNoTodayStudy',
-			'category': '메인',
+			'category': '01_메인',
 			'page': '01_메인_오늘의학습없는경우'
 		},
 		{
 			'stateClass': 'complete',
 			'url': 'main_b2b_todayStudy',
-			'category': '메인',
+			'category': '01_메인',
 			'page': '01_메인_B2B_오늘의 학습'
 		},
 		{
 			'stateClass': 'complete',
 			'url': 'myClassroom',
-			'category': '메인',
+			'category': '01_메인',
 			'page': '01_메인_B2B_나의강의실'
 		},
 		{
 			'stateClass': 'complete',
 			'url': 'myClassroom_noStudy',
-			'category': '메인',
+			'category': '01_메인',
 			'page': '01_메인_B2B_나의강의실_학습이없는경우'
 		},
 		{
 			'stateClass': 'complete',
 			'url': 'todayStudy_noStudy',
-			'category': '메인',
+			'category': '01_메인',
 			'page': '01_메인_B2B_오늘의 학습_학습이없는경우'
 		},
 		{
-			'stateClass': 'waiting',
-			'url': '#',
-			'category': '메인',
+			'stateClass': 'working',
+			'url': 'main_little_homelearn',
+			'category': '01_메인',
 			'page': '01_메인_리틀홈런'
 		},
 
 		{
 			'stateClass': 'complete',
 			'url': 'main_common_contents_pop',
-			'category': '메인',
+			'category': '01_메인',
 			'page': '01_메인_공통_콘텐츠사용만료_팝업_01'
 		},
 
 		{
 			'stateClass': 'complete',
 			'url': 'main_common_contents_pop_02',
-			'category': '메인',
+			'category': '01_메인',
 			'page': '01_메인_공통_콘텐츠사용만료_팝업_02'
 		},
 
 		{
 			'stateClass': 'complete',
 			'url': 'entire_menu_all',
-			'category': '메인',
+			'category': '01_메인',
 			'page': '01_전체메뉴_전과목'
 		},
 
 		{
 			'stateClass': 'complete',
 			'url': 'entire_menu_one',
-			'category': '메인',
+			'category': '01_메인',
 			'page': '01_전체메뉴_한과목'
 		}
 	]
-},
+};
 
-	data.bySubject = {
-		'title': '과목별 화면',
-		'list': [
+data.bySubject = {
+	'title': '02_과목별 화면',
+	'list': [
 
-			{
-				'stateClass': 'complete',
-				'url': 'school_study_basic',
-				'category': '메인',
-				'page': '02_학교공부_기본'
-			},
+		{
+			'stateClass': 'complete',
+			'url': 'school_study_basic',
+			'category': '02_과목별 화면',
+			'page': '02_학교공부_기본'
+		},
 
-			{
-				'stateClass': 'complete',
-				'url': 'school_study_no_banner',
-				'category': '메인',
-				'page': '02_학교공부_배너없는경우'
-			},
+		{
+			'stateClass': 'complete',
+			'url': 'school_study_no_banner',
+			'category': '02_과목별 화면',
+			'page': '02_학교공부_배너없는경우'
+		},
 
-			{
-				'stateClass': 'complete',
-				'url': 'school_study_english',
-				'category': '메인',
-				'page': '02_학교공부_영어'
-			},
+		{
+			'stateClass': 'complete',
+			'url': 'school_study_english',
+			'category': '02_과목별 화면',
+			'page': '02_학교공부_영어'
+		},
 
-			{
-				'stateClass': 'complete',
-				'url': 'schoolStudy_total_entry',
-				'category': '메인',
-				'page': '02_학교공부_중합입문'
-			}
+		{
+			'stateClass': 'complete',
+			'url': 'schoolStudy_total_entry',
+			'category': '02_과목별 화면',
+			'page': '02_학교공부_중합입문'
+		}
 
-		]
-	},
+	]
+};
 
-	data.login = {
-		'title': '로그인',
-		'list': [
+data.login = {
+	'title': '03_로그인',
+	'list': [
 
-			{
-				'stateClass': 'complete',
-				'url': 'b2b_login',
-				'category': '메인',
-				'page': 'B2B_로그인'
-			},
-			{
-				'stateClass': 'complete',
-				'url': 'b2b_login_common_dropdown',
-				'category': '메인',
-				'page': '03_로그인_공통_드롭다운'
-			},
-
-
-			{
-				'stateClass': 'complete',
-				'url': 'login_pop_parent_auth',
-				'category': '메인',
-				'page': '03_로그인_팝업_학부모단말기사용인증'
-			},
-		]
-	},
-
-	data.findId = {
-		'title': '로그인_비밀번호 찾기',
-		'list': [
-
-			{
-				'stateClass': 'complete',
-				'url': 'login_pop_parent_findId',
-				'category': '메인',
-				'page': '04_비밀번호찾기_01_회원정보입력'
-			},
-
-			{
-				'stateClass': 'complete',
-				'url': 'login_pop_admin_findId',
-				'category': '메인',
-				'page': '04_비밀번호찾기_01_B2B_회원정보입력'
-			},
+		{
+			'stateClass': 'complete',
+			'url': 'b2b_login',
+			'category': '03_로그인',
+			'page': 'B2B_로그인'
+		},
+		{
+			'stateClass': 'complete',
+			'url': 'b2b_login_common_dropdown',
+			'category': '03_로그인',
+			'page': '03_로그인_공통_드롭다운'
+		},
 
 
-			{
-				'stateClass': 'complete',
-				'url': 'login_pop_parent_find_pass_more_than_one',
-				'category': '메인',
-				'page': '04_비밀번호찾기_02_자녀가1명이상'
-			},
+		{
+			'stateClass': 'complete',
+			'url': 'login_pop_parent_auth',
+			'category': '03_로그인',
+			'page': '03_로그인_팝업_학부모단말기사용인증'
+		},
+	]
+};
 
-			{
-				'stateClass': 'complete',
-				'url': 'pop_teacher_find_pass_more_than_one',
-				'category': '메인',
-				'page': '04_비밀번호찾기_02_B2B_소속학생이1명이상인경우'
-			},
+data.findId = {
+	'title': '04_로그인_비밀번호찾기',
+	'list': [
 
-			{
-				'stateClass': 'complete',
-				'url': 'pop_no_searched_child',
-				'category': '메인',
-				'page': '04_비밀번호찾기_03_자녀가없는경우'
-			},
+		{
+			'stateClass': 'complete',
+			'url': 'login_pop_parent_findId',
+			'category': '04_로그인_비밀번호찾기',
+			'page': '04_비밀번호찾기_01_회원정보입력'
+		},
 
-			{
-				'stateClass': 'complete',
-				'url': 'pop_findId_error',
-				'category': '메인',
-				'page': '04_비밀번호찾기_04_오류메세지'
-			},
-
-			{
-				'stateClass': 'complete',
-				'url': 'pop_findId_new_pw',
-				'category': '메인',
-				'page': '04_비밀번호찾기_05_새비밀번호입력'
-			},
+		{
+			'stateClass': 'complete',
+			'url': 'login_pop_admin_findId',
+			'category': '04_로그인_비밀번호찾기',
+			'page': '04_비밀번호찾기_01_B2B_회원정보입력'
+		},
 
 
+		{
+			'stateClass': 'complete',
+			'url': 'login_pop_parent_find_pass_more_than_one',
+			'category': '04_로그인_비밀번호찾기',
+			'page': '04_비밀번호찾기_02_자녀가1명이상'
+		},
 
+		{
+			'stateClass': 'complete',
+			'url': 'pop_teacher_find_pass_more_than_one',
+			'category': '04_로그인_비밀번호찾기',
+			'page': '04_비밀번호찾기_02_B2B_소속학생이1명이상인경우'
+		},
 
+		{
+			'stateClass': 'complete',
+			'url': 'pop_no_searched_child',
+			'category': '04_로그인_비밀번호찾기',
+			'page': '04_비밀번호찾기_03_자녀가없는경우'
+		},
 
+		{
+			'stateClass': 'complete',
+			'url': 'pop_findId_error',
+			'category': '04_로그인_비밀번호찾기',
+			'page': '04_비밀번호찾기_04_오류메세지'
+		},
 
+		{
+			'stateClass': 'complete',
+			'url': 'pop_findId_new_pw',
+			'category': '04_로그인_비밀번호찾기',
+			'page': '04_비밀번호찾기_05_새비밀번호입력'
+		},
 
+		{
+			'stateClass': 'complete',
+			'url': 'pop_pw_reset',
+			'category': '04_로그인_비밀번호찾기',
+			'page': '04_비밀번호찾기_06_비밀번호설정완료'
+		},
+	]
+};
 
-		]
-	}
+data.attend = {
+	'title': '05_출석현황_B2B',
+	'list': [
+		{
+			'stateClass': 'complete',
+			'url': 'attend_check_b2b',
+			'category': '05_출석현황_B2B',
+			'page': '05_출석현황_B2B_01'
+		},
+		{
+			'stateClass': 'complete',
+			'url': 'attend_check_b2b_noPlan',
+			'category': '05_출석현황_B2B',
+			'page': '05_출석현황_B2B_02_학습계획없음'
+		},
+	]
+}
 
